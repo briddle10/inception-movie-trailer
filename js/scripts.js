f94e5eb62651f8cdd20a4e2e28733c42cf6a2e4e
Ben Riddle <benriddle10@hotmail.com>
2019-05-21 22:18:43 -0700

    Initial Upload
    * Update of HTML, CSS, JS, ReadMe, and favicons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-console.log('ok');
+$(document).ready(function() {
+    $("#title").lettering();
+});
+
+const player = new Plyr('#player', {
+
+ controls : ['play-large', 'play', 'progress', 'mute', 'volume', 'settings', 'fullscreen'],
+
+
+});
+
+player.poster = 'https://i.imgur.com/SwkPGtU.jpg';
+
+let date = new Date();
+let currentYear = date.getFullYear();
+
+console.log( currentYear );
+console.log( date);
+
+$('footer p span').text(currentYear);
+
+
+
+
+const limit = 10;
+const randNum = Math.floor(Math.random() * limit);
+
+console.log( randNum );
+
+// Background Colors
+$('#totem').addClass('color' + randNum);
